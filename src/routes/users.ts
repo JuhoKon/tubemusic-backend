4edb267f799018f5b530e45ca494437815658ff4
Juho Kontiainen <37773658+JuhoKon@users.noreply.github.com>
2021-05-05 22:16:14 +0300

    Update users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,11 +3,23 @@ const router = express.Router();
 import * as userController from "../controllers/userController";
 import { userValidationRules, uservalidate } from "../middleware/uservalidator";
 import auth from "../middleware/auth";
+import rateLimit from "express-rate-limit";
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 20, // start blocking after 10 reqs
+  message: "Too many requests on this ip.",
+});
 
 router.get("/", auth, userController.index); // ADMIN
 // TODO: ADD DELETE USER
 router.post(
   "/create",
+  loginLimiter,
   userValidationRules(),
   uservalidate,
   userController.create
