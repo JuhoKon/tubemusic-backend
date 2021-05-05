4d85e270401a5ea629e7e585118ba78870bfd3c8
Juho Kontiainen <37773658+JuhoKon@users.noreply.github.com>
2021-05-05 22:17:10 +0300

    Update playlists.ts

--- src/routes/playlists.ts
+++ src/routes/playlists.ts
@@ -3,6 +3,19 @@ const router = express.Router();
 import * as playlist_controller from "../controllers/playlistController";
 
 import auth from "../middleware/auth";
+import rateLimit from "express-rate-limit";
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 min
+  max: 20, // start blocking after 10 reqs
+  message: "Too many requests on this ip.",
+});
+
+
 /* GET users listing. */
 router.get("/", auth, playlist_controller.index); // ADMIN
 router.delete(
@@ -12,7 +25,7 @@ router.delete(
   playlist_controller.deletebyIDHelper
 ); // ADMIN
 
-router.post("/create", auth, playlist_controller.create);
+router.post("/create",loginLimiter,auth, playlist_controller.create);
 
 router.get("/find/:id", auth, playlist_controller.findByID);
 router.put("/update/:id", auth, playlist_controller.updatebyID);
